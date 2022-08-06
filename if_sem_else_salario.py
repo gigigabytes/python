6c1344cd07ff0f94d94e5c5d3bba35014f6e1066
Giovanna Lino <106554500+gigigabytes@users.noreply.github.com>
2022-08-06 19:59:45 -0300

    Delete if_sem_else_salario.py

--- if_sem_else_salario.py
+++
@@ -1,7 +0,0 @@
-salario_base = float(input())
-vendas = float(input())
-meta = float(input())
-salario = salario_base + vendas*0.05 
-if(vendas >= meta):
-    salario = salario + ((vendas-meta)*0.10)
-print("{:.2f}".format(salario))
