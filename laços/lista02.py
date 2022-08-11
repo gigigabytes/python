1575fd8df4db29617fb0186a84dca5341f3e5fff
Giovanna Lino <106554500+gigigabytes@users.noreply.github.com>
2022-08-11 10:48:25 -0300

    Add files via upload

---
+++ laços/lista02.py
@@ -0,0 +1,12 @@
+l = list(map(int,input().split()))
+
+soma = 0
+
+for i in l :
+    print(i)
+    soma = soma + i
+
+print(soma)
+print(sum(l))
+
+#For each = usado de forma moderna para trabalhar com cada elemento de uma lista de elementos
