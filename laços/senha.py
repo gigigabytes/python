899aafec1f0dc7e5d285cc83c824b70d705fdb3d
Giovanna Lino <106554500+gigigabytes@users.noreply.github.com>
2022-08-16 01:06:45 -0300

    Add files via upload

---
+++ laços/senha.py
@@ -0,0 +1,9 @@
+print("Qual a senha?")
+senha = input()
+
+while (senha !="TADS"):
+    print("Senha inválida, tente novamente")
+    apelido = input()
+
+print("Acesso concedido")
+print("O que deseja fazer?")
